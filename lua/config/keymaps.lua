b33226bd4df7b1fbb1f76b33d6bc2cb88f66fa1d
burnchi <1273732363@qq.com>
2024-01-07 22:15:45 +0800

    change keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,16 +17,9 @@ keymap.set("n", "<C-a>", "gg<S-v>G")
 -- Delect one line
 keymap.set({ "i" }, "<C-d>", "<Esc>dd")
 
---insert one line // TODO:CR keymap
--- keymap.set({ "n", "i" }, "<A-CR>", "<Esc>o")
--- keymap.set({ "n", "i" }, "<s-CR>", "<Esc>O")
-
 -- Split window
 keymap.set("n", "ss", ":vsplit<CR>", opts)
 keymap.set("n", "sv", ":split<CR>", opts)
-
--- insert mode cursor go to next line
--- keymap.set({ "i" }, "<C-Enter>", "<Esc>o", opts)
 
 -- 取消高亮
 keymap.set("n", "<leader>nh", ":nohl<CR>")
